b5093f90d65a7847764e2324f1f76e2e9504a903
Alumno <Alumno>
2017-03-28 13:02:02 -0500

    primer commit

---
+++ Pari.Engine/app/src/main/java/com/teclab/pari/pariengine/Image.java
@@ -0,0 +1,27 @@
+package com.teclab.pari.pariengine;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+
+public class Image extends DisplayObject {
+
+       private Bitmap bmp;
+
+
+       public Image(String id, Resources resources) {
+            super(resources);
+            bmp = this.resources.getBmp(id);
+
+       }
+
+ 
+
+       public void onDraw(Canvas canvas) {
+              super.onDraw(canvas);
+              canvas.drawBitmap(bmp, x, y, null);
+
+       }
+
+
+}
